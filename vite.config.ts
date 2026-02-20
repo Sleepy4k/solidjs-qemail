2df9cecbe0c863569645b186875cbc1f212baa1d
Apri Pandu W <sarahpalastrin@gmail.com>
2026-02-20 21:06:31 +0700

    chore(config): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solid from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solid()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "logo": ["src/assets/images/logo.png"],
-          "vendor-solid": ["solid-js", "@solidjs/router"],
-          "vendor-gsap": ["gsap"],
-          "vendor-nprogress": ["nprogress", "src/assets/css/nprogress.css"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [solid()],
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:3000",
+          changeOrigin: true,
         },
       },
     },
-    chunkSizeWarningLimit: 75,
-  },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            "logo": ["src/assets/images/logo.png"],
+            "vendor-solid": ["solid-js", "@solidjs/router"],
+            "vendor-gsap": ["gsap"],
+            "vendor-nprogress": ["nprogress", "src/assets/css/nprogress.css"],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 75,
+    },
+  };
 });
